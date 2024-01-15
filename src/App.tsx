0de02266715dd15bd7005c8bed2036eb26a7ad82
Benedikt Nordhoff <bnord01@gmail.com>
2024-01-15 16:43:00 +0100

    Reintroduce map height workaround for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SidePanel from "./components/SidePanel";
 import { useBoolean } from "@fluentui/react-hooks";
 import MoistureMarkers from "./components/MoistureMarkers";
 import { Info, Imprint } from "./components/Markdown";
+import { useEffect, useState } from "react";
 
 function App() {
     const position: [number, number] = [52.01, 8.542732];
@@ -14,11 +15,19 @@ function App() {
     const [imprintOpen, { setTrue: openImprint, setFalse: dismissImprint }] = useBoolean(false);
     const [infoOpen, { setTrue: openInfo, setFalse: dismissInfo }] = useBoolean(false);
 
+    // We manually update the height of the map to the window.innerHeight to avoid problems with mobile views
+    const [height, setHeight] = useState(window.innerHeight);
+    useEffect(() => {
+        const cb = () => setHeight(window.innerHeight);
+        window.addEventListener("resize", cb);
+        return () => window.removeEventListener("resize", cb);
+    });
+
     return (
         <div className="App">
             <SidePanel isOpen={infoOpen} dismissPanel={() => dismissInfo()} children={<Info />} />
             <SidePanel isOpen={imprintOpen} dismissPanel={() => dismissImprint()} children={<Imprint />} />
-            <MapContainer zoomControl={false} center={position} zoom={zoom} style={{ height: "100vh" }}>
+            <MapContainer zoomControl={false} center={position} zoom={zoom} style={{ height: height }}>
                 <MoistureMarkers />
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
